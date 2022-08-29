f3046196c9bf6b03b57cc82a0570dd455d760024
Kezia Sharnoff <105600498+ksharnoff@users.noreply.github.com>
2022-08-29 09:52:23 -0700

    Update encrypt.go
    removed checking the right key -- instead it just tries to unmarshal

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -10,22 +10,18 @@ import (
  	// for iv generation
  	"io"
  	"crypto/rand"
-
-	// for comparing the key as the correct one
-	"encoding/base64"
 )
-
-const KnownPlaintext = "trans rights R human rights 1234"
-const encryptedPlaintext = "AAAAAAAAAAAAAAAAAAAAAPJmzNkoSo2ojkWHqU9w5GJvQgz8Q6smbAeuB8qNdexf"
+const FileName = "pass.yaml"
+//const FileName = "realPass.yaml"
 
 
 // Makes a key, then a cipher block. It also returns a boolea
 // for if the key is the correct key, by checking with the CorrectKey function. 
 // If the following fucntion is changed, also change it in changeKey.go
-func KeyGeneration(password string) (cipher.Block, bool, string){
+func KeyGeneration(password string) (cipher.Block, string){
 
 	if len([]byte(password)) < 1{
-		return nil, false, "password for key generation is too short, string empty"
+		return nil, "password for key generation is too short, string empty"
 	}
 
 	// Salt generation is going to be the same thing every time. 
@@ -37,23 +33,9 @@ func KeyGeneration(password string) (cipher.Block, bool, string){
 	ciphBlock, err := aes.NewCipher(key)
 
 	if err != nil{
-		return nil, false, err.Error()
+		return nil, err.Error()
 	}
-	return ciphBlock, CorrectKey(ciphBlock), ""
-}
-
-// If the key is correct then it returns true. 
-func CorrectKey(ciphBlock cipher.Block) bool{
-	comparison := Encrypt([]byte(KnownPlaintext), ciphBlock, true)
-
-	encoder := base64.NewEncoding("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890+/")
-	encryptedComp := encoder.EncodeToString(comparison)
-
-	if encryptedComp == encryptedPlaintext{
-		return true
-	}
-
-	return false 
+	return ciphBlock, ""
 }
 
 func Encrypt(plaintext []byte, ciphBlock cipher.Block, keyTest bool) []byte{
