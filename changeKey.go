7cd381337ae4a2170e56c01097409cbb8d0d95e4
Kezia Sharnoff <105600498+ksharnoff@users.noreply.github.com>
2024-08-02 22:07:32 -0700

    Formatting, added instructions, conciser code
    Updated formatting to go standards. Added instructions. Fixed mistake of when to check writeErr. Got rid of unnecessary else statements by changing how errors are handled.

--- changeKey.go
+++ changeKey.go
@@ -1,116 +1,130 @@
 /*
-	This decrypted the file and then reencrypts it with a different password 
+	Decyprts the file and then reencrypts it with a different password
+	(or with different key generation parameters)
 */
 
 package main
 
-import(
+import (
 	"fmt"
-	"os"
 	"gopkg.in/yaml.v3"
+	"os"
 	"pass/encrypt"
 	"strings"
+
+	"crypto/aes"
 	"crypto/cipher"
 	"golang.org/x/crypto/argon2"
-	"crypto/aes"
 	"time"
 )
 
 type entry struct {
-	Name string
-	Tags string
+	Name      string
+	Tags      string
 	Usernames []Field
 	Passwords []Field
 	SecurityQ []Field
-	Notes [6]string 
+	Notes     [6]string
 	Circulate bool
-	Created time.Time
-	Modified time.Time
-	Opened time.Time
+	Created   time.Time
+	Modified  time.Time
+	Opened    time.Time
 }
 type Field struct {
 	DisplayName string
-	Value string
+	Value       string
 }
 
-func main(){
+func main() {
 	entries := []entry{}
 
+	fmt.Println("\nFirst you will give your old password, then your new one.\nIf you are just changing the key parameters, write the same password twice.\n\nIf you would like to change the key parameters go into the changeKey.go file and:\n\tChange keyGenChange to be true\n\tMake the keyGeneration function the same as it is in encrypt.go and:\nNext, go into encrypt.go\n\tChange the keyGeneration function to have different parameter\n")
+
 	var oldPass string
-	fmt.Println("write your old password: ")
+	fmt.Println("Write your old password: ")
 	fmt.Scan(&oldPass)
 	fmt.Print("\033[F\r", strings.Repeat(" ", len(oldPass)))
 	fmt.Println("")
 
 	var newPass string
-	fmt.Println("write your new password: ")
+	fmt.Println("Write your new password: ")
 	fmt.Scan(&newPass)
 	fmt.Print("\033[F\r", strings.Repeat(" ", len(newPass)))
 	fmt.Println("")
 
-	// if this is set to true then it will make the key to decrypt 
-	// using the keygeneration function in this file, which will have
-	// the old parameters to change away from
-	keyGenChange := false 
+	fmt.Println("THINGS ARE HAPPENING - DO NOT QUIT THE PROGRAM\n")
 
-	var ciphBlockOld cipher.Block 
-	var strOld string
+	// if this is set to true then it will make the key to decrypt
+	// using the keyGeneration function in this file, which will have
+	// the old parameters to change away from. if you are just changing
+	// what the password is and not the parameters, keep it as false
+	keyGenChange := false
 
-	if keyGenChange{
-		ciphBlockOld, strOld = keyGeneration(oldPass)
-	}else{
-		ciphBlockOld, strOld = encrypt.KeyGeneration(oldPass)
+	var ciphBlockOld cipher.Block
+	var oldKeyErr string
+
+	if keyGenChange {
+		ciphBlockOld, oldKeyErr = keyGeneration(oldPass)
+	} else {
+		ciphBlockOld, oldKeyErr = encrypt.KeyGeneration(oldPass)
 	}
-	if strOld == ""{
-		input, inputErr := os.ReadFile(encrypt.FileName)
-		if inputErr != nil{
-			fmt.Println("error in os.ReadFile \n", inputErr.Error())
-		}else{
-			decryptedInput := encrypt.Decrypt(input, ciphBlockOld)
 
-			unmarshErr := yaml.Unmarshal(decryptedInput, &entries)
-			if unmarshErr != nil{
-				fmt.Println("error in yaml.Unmarshal, wrong password mayhaps? \nor check that keyGenChange is false or true as needed \n", unmarshErr.Error())
-			}else{
-				fmt.Println("successfully unmarshaled the input, success so far.")	
-
-				ciphBlockNew, strNew := encrypt.KeyGeneration(newPass)
-
-				if strNew != ""{
-					fmt.Println(" erroro!!" + strNew)
-				}else{
-					output, marshErr := yaml.Marshal(entries)
-					if marshErr != nil{
-						fmt.Println("error in yaml.marshal the entries \n", marshErr.Error())
-					}else{
-						encryptedOutput := encrypt.Encrypt(output, ciphBlockNew, false)
-
-						writeErr := os.WriteFile(encrypt.FileName+".tmp", encryptedOutput, 0600)
-						os.Rename(encrypt.FileName+".tmp", encrypt.FileName)
-
-						if writeErr != nil{
-							fmt.Println("error in os.writeFile \n", writeErr.Error())
-						}else{
-							fmt.Println("success! changed the password, wrote to the file!")
-						}
-					}
-				}
-			}
-		}
-	}else{
-		fmt.Println("errror: " + strOld)
+	if oldKeyErr != "" {
+		printAndExit("Error in key generation of old password: " + oldKeyErr)
 	}
+
+	input, readErr := os.ReadFile(encrypt.FileName)
+
+	if readErr != nil {
+		printAndExit("Error in reading from file: " + readErr.Error())
+	}
+
+	decryptedInput := encrypt.Decrypt(input, ciphBlockOld)
+
+	unmarshErr := yaml.Unmarshal(decryptedInput, &entries)
+
+	if unmarshErr != nil {
+		printAndExit("Error in unmarshaling.\nThis could be from a wrong password.\nOr, check if the bool keyGenChange is true or false as needed.\n " + unmarshErr.Error())
+	}
+
+	fmt.Println("Decrypted & unmarshled the input, success so far!")
+	fmt.Println("THINGS ARE HAPPENING - DO NOT QUIT THE PROGRAM\n")
+
+	ciphBlockNew, newKeyErr := encrypt.KeyGeneration(newPass)
+
+	if newKeyErr != "" {
+		printAndExit("Error in key generation of new password: " + newKeyErr)
+	}
+
+	output, marshErr := yaml.Marshal(entries)
+
+	if marshErr != nil {
+		printAndExit("Error in marshaling: " + marshErr.Error())
+	}
+	
+	encryptedOutput := encrypt.Encrypt(output, ciphBlockNew)
+
+	writeErr := os.WriteFile(encrypt.FileName + ".tmp", encryptedOutput, 0600)
+
+	if writeErr != nil {
+		printAndExit("Error in writing to file: " + writeErr.Error())
+	}
+
+	os.Rename(encrypt.FileName + ".tmp", encrypt.FileName)
+
+	fmt.Println("Success! The passwords have been re-encrypted and written to file!")
 }
 
 // This is different than KeyGeneration in encrypt.go only so that
 // this can be used to decrypt the file initially with parameters
-// different than in pass/encrypt. So if you want to change the 
-// parameters, have the old ones here and the new ones you want to 
-// change in encrypt.go. 
-func keyGeneration(password string) (cipher.Block, string){
-	if len([]byte(password)) < 1{
+// different than in pass/encrypt. So if you want to change the
+// parameters, have the old ones here and the new ones you want to
+// change in encrypt.go.
+func keyGeneration(password string) (cipher.Block, string) {
+	if len([]byte(password)) < 1 {
 		return nil, "password for key generation is too short, string empty"
 	}
+	
 	salt := []byte("qwertyuiopasdfghjklzxcvbnm")
 
 	// parameters currently in encrypt.go are: 4, 2048*1024, 4, 32
@@ -118,8 +132,14 @@ func keyGeneration(password string) (cipher.Block, string){
 
 	ciphBlock, err := aes.NewCipher(key)
 
-	if err != nil{
+	if err != nil {
 		return nil, err.Error()
 	}
 	return ciphBlock, ""
+}
+
+
+func printAndExit(error string) {
+	fmt.Println(error)
+	os.Exit(1)
 }
